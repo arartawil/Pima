3fc87a34baee3a00c5e6c296ac19964e806dac91
arartawil <arartawil@yahoo.com>
2022-03-04 17:54:08 +0300

    update

--- Pima-app.py
+++ Pima-app.py
@@ -22,22 +22,15 @@ if uploaded_file is not None:
 else:
     def user_input_features():
 
-        Pregnancies = st.sidebar.slider('Pregnancies', 0, 17, 10)
+
         Glucose = st.sidebar.slider('Glucose', 0, 199, 100)
         BloodPressure = st.sidebar.slider('Blood Pressure', 0, 122, 50)
-        SkinThickness = st.sidebar.slider('Skin Thickness', 0, 99, 10)
-        Insulin = st.sidebar.slider('Insulin', 0, 846, 100)
         BMI = st.sidebar.slider('Body Mass Index', 10.0 ,67.1 ,17.2)
-        DiabetesPedigreeFunction = st.sidebar.slider('Diabetes Pedigree (Family history)', 0.08, 2.42, 1.0)
         Age = st.sidebar.slider('Age', 10, 80, 60)
         data = {
-                'Pregnancies': Pregnancies,
                 'Glucose': Glucose,
                 'BloodPressure': BloodPressure,
-                'SkinThickness': SkinThickness,
-                'Insulin': Insulin,
                 'BMI': BMI,
-                'DiabetesPedigreeFunction': DiabetesPedigreeFunction,
                 'Age': Age,
         }
         features = pd.DataFrame(data, index=[0])
@@ -53,11 +46,8 @@ else:
 # Displays the user input features
 st.subheader('User Input features')
 
-if uploaded_file is not None:
-    st.write(input_df)
-else:
-    st.write('Awaiting CSV file to be uploaded. Currently using example input parameters (shown below).')
-    st.write(input_df)
+
+st.write(input_df)
 
 # Reads in saved classification model
 load_clf = pickle.load(open('Pima_clf.pkl', 'rb'))
@@ -67,9 +57,9 @@ prediction = load_clf.predict(input_df)
 prediction_proba = load_clf.predict_proba(input_df)
 
 
-st.subheader('Prediction')
+
 penguins_species = np.array(['Negative','Positive'])
-st.write(penguins_species[prediction])
+st.subheader('Your Result : ' +penguins_species[prediction][0])
 
 st.subheader('Prediction Probability')
 st.write(prediction_proba)
